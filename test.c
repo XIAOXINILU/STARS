730cd7935b4ef696f2fafa805504d5ec7786ad1c
C <121746411+XIAOXINILU@users.noreply.github.com>
2023-03-28 21:05:04 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", !a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	char c = 't';
+//	char* p = &a;
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(a));//4
+//	printf("%d\n", sizeof(int));
+//
+//	printf("%d\n", sizeof(c));//1
+//	printf("%d\n", sizeof(char));
+//
+//	printf("%d\n", sizeof(p));//8
+//	printf("%d\n", sizeof(char*));
+//
+//
+//	printf("%d\n", sizeof(arr));//40
+//	printf("%d\n", sizeof(int [10]));
+//
+//	printf("%d\n", sizeof(short));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	short s = 0;
+//	int a = 10;
+//	printf("%d\n", sizeof(a + 5));
+//	printf("%d\n", sizeof(s = a + 5));//a+5还是整型
+//	printf("%d\n", sizeof(s=a));
+//
+//
+//	printf("%d\n", s);
+//	return 0;
+//}
+
+//   ~   按位取反
+int main()
+{
+	int a = 0;
+	printf("%d\n", ~a);
+	return 0;
+}
